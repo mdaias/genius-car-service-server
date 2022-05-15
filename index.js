43b2257efe31dae54a399697eff1a23416456673
mdaias <mdaias29@gmail.com>
2022-05-16 02:45:33 +0600

    order post and get orders

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order')
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -48,7 +49,23 @@ async function run() {
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
         });
+        
+        //post order
+        app.post('/order', async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
 
+        //get orders
+        app.get('/orders', async(req, res)=>{
+            const email = req.query.email;
+            
+            const query = {email:email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
     }
     finally {
 
